7f5f48d05c11dc8379db58f25b46fd8c15f6cdd8
Robert Wood <robert.rd.wood@gmail.com>
2019-05-12 19:34:32 -0400

    Update to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,21 +2,48 @@
 var tableData = data;
 var tbody = d3.select("tbody");
 
-data.forEach(function(ufoReport) {
+tableData.forEach(writeTable);
+    
+function writeTable(ufoReport) {
+
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(function([key, value]) {
-      // Append a cell to the row for each value
-      // in the UFO object
-      var cell = row.append("td");
-      cell.text(value);
+        // Append a cell to the row for each value
+        // in the UFO object
+        var cell = row.append("td");
+        cell.text(value);
     });
-  });
+}
+    
+function filterTable(ufoReport) {
+
+}
 
 var filterButton = d3.select("#filter-btn");
 
-function handleChange(event) {
-    var inputValue = d3.event.target.value;
-    console.log(inputValue);
+function handleFilter(event) {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    tableData.forEach(function(ufoReport) {
+    // If function is called with a date argument, display matching date records
+        if (Object.entries(ufoReport)[0][1] == inputValue) {
+            var row = tbody.append("tr");
+            Object.entries(ufoReport).forEach(function([key, value]) {
+                // Append a cell to the row for each value
+                // in the UFO object
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        }
+    });
 }
 
-filterButton.on("click",handleChange);
+filterButton.on("click",handleFilter);
