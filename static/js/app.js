c12639074176a4f83cf61bbd4ff19e94dcba0d80
Robert Wood <robert.rd.wood@gmail.com>
2019-05-14 17:33:26 -0400

    Update to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -5,9 +5,13 @@ var tbody = d3.select("tbody");
 // Write full table
 tableData.forEach(writeTable);
 
+// Write row and columns
 function writeTable(ufoReport) {
 
+    // Append row element
     var row = tbody.append("tr");
+
+    // Loop through each key-value pair
     Object.entries(ufoReport).forEach(function([key, value]) {
         // Append a cell to the row for each value
         // in the UFO object
@@ -15,9 +19,6 @@ function writeTable(ufoReport) {
         cell.text(value);
     });
 }
-    
-// Declare variable for Filter button
-var filterButton = d3.select("#filter-btn");
 
 // Function to handle Filter
 function handleFilter(event) {
@@ -48,5 +49,27 @@ function handleFilter(event) {
     });
 }
 
-// Define my button
+function handleClear(event) {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Delete current table
+    d3.selectAll("tbody>tr").remove();
+
+    // Write full table
+    tableData.forEach(writeTable);
+
+}
+
+// Declare variable for Filter Table button
+var filterButton = d3.select("#filter-btn");
+
+// Declare variable for Clear Filter button
+var clearButton = d3.select("#clear-btn");
+
+// Define Filter Table button action
 filterButton.on("click",handleFilter);
+
+// Define Clear Filter button action
+clearButton.on("click",handleClear);
