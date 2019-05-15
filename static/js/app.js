fd2ae954c7e6f2b704537c38934205d616d87b4b
Robert Wood <robert.rd.wood@gmail.com>
2019-05-15 15:03:18 -0400

    Update app.js to handle all select dropdowns

--- static/js/app.js
+++ static/js/app.js
@@ -22,11 +22,14 @@ function writeTable(ufoReport) {
         cell.text(value);
     });
 }
+//////////////////////////////////////////
+// State handling and select dropdown
+//////////////////////////////////////////
 
 // Create array of states
 var stateArray = [];
 
-for (var i=0; i<tableData.length; i+=1) {
+for (var i=0; i<tableData.length; i++) {
     // Declare current state variable and set equal to the corresponding element in the object
     var currentState = tableData[i].state;
     // If the current state is not already in the array, add it
@@ -38,13 +41,82 @@ for (var i=0; i<tableData.length; i+=1) {
 // Sort stateArray alphabetically
 stateArray.sort();
 
-console.log(stateArray);
+// Map uppercase state abbreviations to new array
+var stateArrayCaps = stateArray.map(function(item) {
+    return item.toUpperCase();
+});
 
 // Populate State select dropdown
-stateArray.forEach(function(state) {
+stateArrayCaps.forEach(function(state) {
     var option = stateName.append("option");
     option.text(state);
 });
+
+//////////////////////////////////////////
+// Country handling and select dropdown
+//////////////////////////////////////////
+
+// Create array of countries
+var countryArray = [];
+
+for (var i=0; i<tableData.length; i++) {
+    // Declare current country variable and set equal to the corresponding element in the object
+    var currentCountry = tableData[i].country;
+    // If the current country is not already in the array, add it
+    if (countryArray.includes(currentCountry) == false) {
+        countryArray.push(currentCountry);
+    }
+}
+
+// Sort countryArray alphabetically
+countryArray.sort();
+
+// Map uppercase country abbreviations to new array
+var countryArrayCaps = countryArray.map(function(item) {
+    return item.toUpperCase();
+});
+
+// Populate Country select dropdown
+countryArrayCaps.forEach(function(country) {
+    var option = countryName.append("option");
+    option.text(country);
+});
+
+
+//////////////////////////////////////////
+// Shape handling and select dropdown
+//////////////////////////////////////////
+
+// Create array of shapes
+var shapeArray = [];
+
+for (var i=0; i<tableData.length; i++) {
+    // Declare current shape variable and set equal to the corresponding element in the object
+    var currentShape = tableData[i].shape;
+    // If the current shape is not already in the array, add it
+    if (shapeArray.includes(currentShape) == false) {
+        shapeArray.push(currentShape);
+    }
+}
+
+// Sort shapeArray alphabetically
+shapeArray.sort();
+
+// Create array to store shapes converted to Camel case
+var shapeArrayCamel = [];
+
+// Capitalize the first letter of each array element
+for(var i = 1 ; i < shapeArray.length ; i++) {
+    shapeArrayCamel[i] = shapeArray[i].charAt(0).toUpperCase() + shapeArray[i].substr(1);
+}       
+
+// Populate Shape select dropdown
+shapeArrayCamel.forEach(function(shape) {
+    var option = shapeName.append("option");
+    option.text(shape);
+});
+
+
 
 // Function to handle Filter
 function handleFilter(event) {
